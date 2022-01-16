d2a2263d5f8ba4d2e7d9012d5cd29ceab6126e59
Maciej Falbogowski <maciej.falbogowski@gmail.com>
2022-01-16 17:33:05 +0100

    Initial commit

---
+++ Falbogowski.Memory.INTERFACES/IGameCard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Falbogowski.Memory.INTERFACES
+{
+    public interface IGameCard
+    {
+        public int Get
+    }
+}
